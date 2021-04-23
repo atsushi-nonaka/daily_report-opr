a5e835d911bd3d2c09aa5c7d04e6658d2721c1ad
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2021-04-23 09:59:09 +0900

    add method_name

--- sources/handle_report.lua
+++ sources/handle_report.lua
@@ -1,2 +1,13 @@
-local res = ngx.location.capture("/nginx_report", { method=ngx.HTTP_GET })
-return ngx.say(res.body)
+local method_name = ngx.req.get_method()
+
+if method_name == 'GET'
+    local res = ngx.location.capture('/nginx_report', { method=ngx.HTTP_GET })
+    if res.status == 200 then
+        return ngx.say(res.body)
+    else
+        return ngx.say("Can't connect /nginx_report")
+    end
+else if method_name == 'POST'
+    
+else if method_name == 'PUT' 
+else if method_name == 'DELETE'     
