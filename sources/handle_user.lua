fcf990af3f02b930634626206157b16998fe8e5a
atsushi-nonaka <atsushi.nonaka@rakus-partners.co.jp>
2021-04-25 11:12:14 +0900

    add post for user

--- sources/handle_user.lua
+++ sources/handle_user.lua
@@ -1,14 +1,23 @@
 local method_name = ngx.req.get_method()
 
-if method_name == 'GET'
+if method_name == 'GET' then
     local res = ngx.location.capture('/nginx_user', { method=ngx.HTTP_GET })
     if res.status == 200 then
         return ngx.say(res.body)
     else
         return ngx.say("Can't connect /nginx_user")
     end
-else if method_name == 'POST'
-    
-else if method_name == 'PUT' 
-else if method_name == 'DELETE'
+else if method_name == 'POST' then
+    ngx.req.read_body()
+    _args = ngx.req.get_body_data()
+
+    local res = ngx.location.capture('/nginx_report', { 
+        method = ngx.HTTP_POST,
+        args = _args
+    })
+    if res.status == 200 then
+        return ngx.say(res.body)
+    else
+        return ngx.say("Can't connect /nginx_report")
+    end
 end
